6515ded4aae05bb0876f0d52922c113c49d1d3de
HashimPario <hashimpario@gmail.com>
2024-08-02 20:06:23 +0500

    connection issues resolved

--- db/index.js
+++ db/index.js
@@ -1,24 +1,56 @@
+// const mongoose = require("mongoose");
+
+
+
+// const connectDb = () => {
+//     mongoose
+//   .connect(
+//     process.env.MONGO_URI ||
+//     "mongodb+srv://hashimpario:Hashim123@cluster0.goaqtkk.mongodb.net/parkingapp?retryWrites=true&w=majority&appName=Cluster0",
+      
+//     {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     }
+//   )
+//   .then(() => {
+//     console.log("MongoDb Connected!");
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//   });
+// }
+
+// module.exports = connectDb;
+
+
+
+// db.js
 const mongoose = require("mongoose");
 
+// Securely get the MongoDB URI from environment variables
+const mongoURI = process.env.MONGO_URI || "your-default-mongo-uri-here";
 
-
-const connectDb = () => {
-    mongoose
-  .connect(
-    process.env.MONGO_URI ||
-    "mongodb+srv://hashimpario:Hashim123@cluster0.goaqtkk.mongodb.net/parkingapp?retryWrites=true&w=majority&appName=Cluster0",
-      
-    {
+const connectDb = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log("MongoDb Connected!");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-}
+      poolSize: 10, // Adjust the pool size as needed
+    });
+    console.log("MongoDB Connected!");
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err.message);
+  }
+};
 
-module.exports = connectDb;
+const disconnectDb = async () => {
+  try {
+    await mongoose.disconnect();
+    console.log("MongoDB Disconnected!");
+  } catch (err) {
+    console.error("Error disconnecting from MongoDB:", err.message);
+  }
+};
+
+module.exports = { connectDb, disconnectDb };
--- server.js
+++ server.js
@@ -1,18 +1,54 @@
-const express = require('express')
-const cors = require('cors')
-const connectDb = require("./db");
-const app = express()
+// const express = require('express')
+// const cors = require('cors')
+// const connectDb = require("./db");
+// const app = express()
 
 
-app.use(express.json())
-app.use(cors())
+// app.use(express.json())
+// app.use(cors())
 
-app.use('/', require('./routes/userRouter'))
+// app.use('/', require('./routes/userRouter'))
 
+// connectDb();
+
+
+// const PORT = process.env.PORT || 5000
+// app.listen(PORT,()=>{
+//     console.log('Server is running on port',PORT)
+// })
+
+
+
+// server.js
+const express = require('express');
+const cors = require('cors');
+const { connectDb, disconnectDb } = require("./db");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', require('./routes/userRouter'));
+
+// Connect to the database
 connectDb();
 
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+  console.log('Server is running on port', PORT);
+});
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT,()=>{
-    console.log('Server is running on port',PORT)
-})
+// Graceful shutdown function
+const gracefulShutdown = async (signal) => {
+  console.log(`${signal} received. Shutting down gracefully...`);
+  server.close(async () => {
+    console.log("Closing server...");
+    await disconnectDb();
+    process.exit(0);
+  });
+};
+
+// Handle termination signals
+process.on('SIGINT', () => gracefulShutdown('SIGINT'));
+process.on('SIGTERM', () => gracefulShutdown('SIGTERM'));
