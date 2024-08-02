09f6a439370d032835b5ab1941fa89bb7812a0e8
HashimPario <hashimpario@gmail.com>
2024-08-02 20:58:11 +0500

    02-aug

--- db/index.js
+++ db/index.js
@@ -1,56 +1,26 @@
-// const mongoose = require("mongoose");
-
-
-
-// const connectDb = () => {
-//     mongoose
-//   .connect(
-//     process.env.MONGO_URI ||
-//     "mongodb+srv://hashimpario:Hashim123@cluster0.goaqtkk.mongodb.net/parkingapp?retryWrites=true&w=majority&appName=Cluster0",
-      
-//     {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("MongoDb Connected!");
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-// }
-
-// module.exports = connectDb;
-
-
-
-// db.js
 const mongoose = require("mongoose");
 
-// Securely get the MongoDB URI from environment variables
-const mongoURI = process.env.MONGO_URI || "your-default-mongo-uri-here";
 
-const connectDb = async () => {
-  try {
-    await mongoose.connect(mongoURI, {
+
+const connectDb = () => {
+    mongoose
+  .connect(
+    process.env.MONGO_URI ||
+    "mongodb+srv://hashimpario:Hashim123@cluster0.goaqtkk.mongodb.net/parkingapp?retryWrites=true&w=majority&appName=Cluster0",
+      
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      poolSize: 10, // Adjust the pool size as needed
-    });
-    console.log("MongoDB Connected!");
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err.message);
-  }
-};
+    }
+  )
+  .then(() => {
+    console.log("MongoDb Connected!");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+}
 
-const disconnectDb = async () => {
-  try {
-    await mongoose.disconnect();
-    console.log("MongoDB Disconnected!");
-  } catch (err) {
-    console.error("Error disconnecting from MongoDB:", err.message);
-  }
-};
+module.exports = connectDb;
 
-module.exports = { connectDb, disconnectDb };
+
